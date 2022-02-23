917ac7a83ba873604ea482f87efe7b9327f2b877
Dwarikanath1502 <ddwarikaanth15@gmail.com>
2022-02-23 10:30:32 -0500

    updated favicon and title

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -2,15 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import { useState } from 'react';
+import Alert from './components/Alert';
+// import About from './components/About'; 
 
 function App() {
+
+  const [mode, setmode] = useState('light')
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setmode('dark');
+      document.body.style.backgroundColor = '#2c1717';
+      showAlert("Dark mode has been enabled!", "Success")
+    } else {
+      setmode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled!", " ")
+
+    }
+  }
   return (
     <>
 
-      < Navbar title="TextUtils" />
+      < Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className='container my-3'>
-        <TextForm heading = "Enter the text to analyze "/>
+        <TextForm heading="Enter the text to analyze" mode={mode} />
         {/* <About /> */}
       </div>
     </>
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react'
 export default function TextForm(props) {
 
     const [text, setText] = useState("")
-
+    
     //fun to convert uppercase
     const handleUpClick = () => {
         console.log("Upercase was clicked : " + text);
@@ -39,7 +39,7 @@ export default function TextForm(props) {
     //handle extra space
     const handleExtraSpace = () => {
         let newText = text.split(/[ ]+/);
-        setText(newText.join(" ")) 
+        setText(newText.join(" "))
     }
 
     const handleOnChange = (event) => {
@@ -49,10 +49,13 @@ export default function TextForm(props) {
 
     return (
         <>
-            <div className='container'>
+            <div className='container' style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" style={{
+                        backgroundColor: props.mode === 'dark' ? 'grey' : 'white',
+                        color: props.mode === 'dark' ? 'white' : 'black'
+                    }} value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
                 </div>
                 <button className='btn btn-primary mx-1' onClick={handleUpClick} >Uppercase</button>
                 <button className='btn btn-primary mx-1' onClick={handleLowClick}>Lowercase</button>
@@ -61,12 +64,13 @@ export default function TextForm(props) {
                 <button className='btn btn-primary mx-1' onClick={handleExtraSpace}>Remove Extra Space</button>
                 <button className='btn btn-danger mx-1' onClick={handleClearClick}>Clear</button>
             </div>
-            <div className="container my-3">
+            <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h2>Your text summary:</h2>
                 <p> {text.split(" ").length} words and {text.length} characters</p>
-                <p> {0.008 * text.split(" ").length} minutes read</p>
+                <p> {0.008 * text.length>0?text.trim().split(" ").length : 0} minutes read</p>
                 <h2>Preview</h2>
-                <p>{text}</p>
+                {/* <p>{text.length > 0 ? text : "Your text preview will appear here!"}</p> */}
+                <p>{text===''? "Enter your text to preview" : text}</p>
             </div>
         </>
     )
